c5ad867e42d4c3c6d80149ffe7472413081f6be5
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-08-02 23:13:45 +0500

    services dynamic

--- components/services/Services.tsx
+++ components/services/Services.tsx
@@ -1,3 +1,4 @@
+import services from "@/settings/data/services";
 import Container from "../Container";
 import CtaBtn from "../CtaBtn";
 import Offer from "./Offer";
@@ -10,22 +11,14 @@ const Services = () => {
           What We Offer
         </h2>
         <div className="flex flex-col gap-[10px]">
-          <Offer
-            initiallyOpen
-            title="Tailored for you brand"
-            description="Get a tailor-made website that perfectly reflect your brand and objectives."
-            standout="Ensuring your utmost satisfaction with unlimited revisions until you're thrilled with the final result."
-          />
-          <Offer
-            title="Fast and Secure"
-            description="Fast-loading and secure solutions that will instill trust in your visitors."
-            standout="Ensuring improved online reputation"
-          />
-          <Offer
-            title="Interactive & Engaging User Experience"
-            description="Interactive solutions that deliver captivating user experiences, leaving a lasting impression on your visitors."
-            standout="Elevating engagement with seamless integration of multimedia, stunning visuals, videos and animations."
-          />
+          {services.map((offer, index) => (
+            <Offer
+              initiallyOpen={index === 0}
+              title={offer.title}
+              description={offer.description}
+              standout={offer.standout}
+            />
+          ))}
         </div>
         <div className="flex flex-col gap-[54px] items-center">
           <h3 className="font-SATOSHI font-bold text-GRAYBLUE text-[22px]">
---
+++ settings/data/services.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "Tailored for you brand",
+    description:
+      "Get a tailor-made website that perfectly reflect your brand and objectives.",
+    standout:
+      "Ensuring your utmost satisfaction with unlimited revisions until you're thrilled with the final result.",
+  },
+  {
+    title: "Fast and Secure",
+    description:
+      "Fast-loading and secure solutions that will instill trust in your visitors.",
+    standout: "Ensuring improved online reputation.",
+  },
+  {
+    title: "Interactive & Engaging User Experience",
+    description:
+      "Interactive solutions that deliver captivating user experiences, leaving a lasting impression on your visitors.",
+    standout:
+      "Elevating engagement with seamless integration of multimedia, stunning visuals, videos and animations.",
+  },
+];
